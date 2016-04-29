1cbaa296deaa86af1883af1405deb10b3a203336
Matt Gumbley <matt.gumbley@devzendo.org>
2016-04-29 08:12:51 +0100

    Tidy up.

--- dxclusterwatch-cmd/src/main/java/org/devzendo/dxclusterwatch/cmd/DXClusterWatch.java
+++ dxclusterwatch-cmd/src/main/java/org/devzendo/dxclusterwatch/cmd/DXClusterWatch.java
@@ -1,7 +1,6 @@
 package org.devzendo.dxclusterwatch.cmd;
 
 import java.io.File;
-import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.devzendo.commoncode.concurrency.ThreadUtils;
@@ -11,23 +10,20 @@ import org.slf4j.LoggerFactory;
 public class DXClusterWatch {
 	private static final Logger LOGGER = LoggerFactory.getLogger(DXClusterWatch.class);
 	
+	private final AtomicBoolean running = new AtomicBoolean(true);
+	private final Config config;
 	private final Persister persister;
 	private final PageBuilder pageBuilder;
 	private final Tweeter tweeter;
 	private final SitePoller sitePoller;
-	
-	private final AtomicBoolean running = new AtomicBoolean(true);
-
-	private final Config config;
-
 
 	public DXClusterWatch(final File prefsDir, final Config config, final Persister persister, final PageBuilder pageBuilder, final Tweeter tweeter) {
 		this.config = config;
 		this.persister = persister;
 		this.pageBuilder = pageBuilder;
 		this.tweeter = tweeter;
-		final Set<String> callsigns = config.getCallsigns();
-		sitePoller = new DXClusterSitePoller(prefsDir, config);
+		this.sitePoller = new DXClusterSitePoller(prefsDir, config);
+
 		Signals.withHandler(new Runnable() {
 			@Override
 			public void run() {
@@ -41,8 +37,10 @@ public class DXClusterWatch {
 		long tweetBackoffCount = 0;
 		long nextPollTime = nowSeconds(); // force the first poll.
 		long nextTweet = nowSeconds(); // force the first tweet to happen now
+		// transient counts of tweets and rebuilds...
 		int tweetNumber = 1;
 		int pageRebuildNumber = 1;
+		
 		LOGGER.info("Starting....");
 		while (running.get()) {
 			final int pollSeconds = config.getPollMinutes() * 60;
